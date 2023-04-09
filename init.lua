d4fd9e2d4a04d670a32ff6b821382641d8c175e8
baw <otting.8@osu.edu>
2023-04-08 21:39:16 -0500

    improve: update lsp config and move into separate file

--- init.lua
+++ init.lua
@@ -1,48 +1,7 @@
 -- Load the related files
 require('base')
 require('plugins')
-
--- File config
-vim.filetype.add({
-  extension = {
-    conf = 'dosini',
-    avsc = 'json',
-    schema = 'json',
-    proto = 'proto',
-    peg = 'go'
-  }
-})
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "html",
-  command = "setlocal ts=2 sw=2 expandtab"
-})
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "javascript",
-  command = "setlocal ts=4 sw=4 sts=0 noexpandtab"
-})
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "python",
-  command = "setlocal ts=4 sw=4 sts=4"
-})
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "json",
-  command = "setlocal ts=2 sw=2 sts=2"
-})
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "proto",
-  command = "setlocal ts=2 sw=2 sts=2 expandtab"
-})
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "lua",
-  command = "setlocal ts=2 sw=2 sts=2 expandtab"
-})
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = "*.py",
-  callback = function()
-    vim.cmd("silent! !python -m black %")
-    vim.cmd("silent! !python -m isort %")
-  end
-})
+require('lsp')
 
 -- Shortcut Commands
 vim.keymap.set('n', '<leader>b', "<cmd>NvimTreeToggle<cr>", {noremap = true})
@@ -59,18 +18,3 @@ vim.diagnostic.config({
 })
 vim.o.updatetime = 250
 vim.cmd("autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})")
-
--- Golang config
-require 'lspconfig'.gopls.setup{}
-
--- Python config
-local lsp = require('lspconfig')
--- local completion = require('cmp')
-
-local custom_attach = function()
-  -- completion.on_attach()
-  -- Python isn't setting omnifunc correctly??
-  vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-end
-
-lsp.pylsp.setup{ on_attach = custom_attach }
---
+++ lua/lsp.lua
@@ -0,0 +1,87 @@
+local lsp = require('lspconfig')
+
+-- Golang config
+lsp.gopls.setup{}
+
+-- Python config
+lsp.pylsp.setup{}
+
+-- https://github.com/neovim/nvim-lspconfig#Suggested-configuration
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
+
+-- File config
+vim.filetype.add({
+  extension = {
+    conf = 'dosini',
+    avsc = 'json',
+    schema = 'json',
+    proto = 'proto',
+    peg = 'go'
+  }
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "html",
+  command = "setlocal ts=2 sw=2 expandtab"
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "javascript",
+  command = "setlocal ts=4 sw=4 sts=0 noexpandtab"
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  command = "setlocal ts=4 sw=4 sts=4"
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "json",
+  command = "setlocal ts=2 sw=2 sts=2"
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "proto",
+  command = "setlocal ts=2 sw=2 sts=2 expandtab"
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "lua",
+  command = "setlocal ts=2 sw=2 sts=2 expandtab"
+})
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*.py",
+  callback = function()
+    vim.cmd("silent! !python -m black %")
+    vim.cmd("silent! !python -m isort %")
+  end
+})
