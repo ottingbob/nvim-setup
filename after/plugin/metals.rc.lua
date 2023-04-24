c62fe821077a6344d4e820f78fa9ae4996bb84ad
baw <otting.8@osu.edu>
2023-04-23 23:56:13 -0500

    improve(metals): add in lsp status for metals

--- after/plugin/metals.rc.lua
+++ after/plugin/metals.rc.lua
@@ -8,6 +8,8 @@ metals_config.init_options.statusBarProvider = "on"
 -- Example settings
 metals_config.settings = {
   showImplicitArguments = true,
+  showImplicitConversionsAndClasses = true,
+  showInferredType = true,
   excludedPackages = {},
 }
 
@@ -20,3 +22,24 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
   group = nvim_metals_group
 })
+
+-- Enable LSP status for metals
+-- https://github.com/hudclark/.dotfiles/blob/main/.config/nvim/fnl/user/plugins/lsp/metals.fnl
+local function metals_status_handler(err, status, ctx)
+  local val = {}
+  -- trim and remove spinner
+  local text = status.text:gsub('[⠇⠋⠙⠸⠴⠦]', ''):gsub("^%s*(.-)%s*$", "%1")
+  if status.hide then
+    val = {kind = "end"}
+  elseif status.show then
+    val = {kind = "begin", title = text}
+  elseif status.text then
+    val = {kind = "report", message = text}
+  else
+    return
+  end
+  local info = {client_id = ctx.client_id}
+  local msg = {token = "metals", value = val}
+  vim.lsp.handlers["$/progress"](err, msg, ctx)
+end
+metals_config.handlers = {['metals/status'] = metals_status_handler}
