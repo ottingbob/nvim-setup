617e7b7a50eb228ce35f9c28fdd8e2b9392bb751
baw <otting.8@osu.edu>
2024-03-19 15:29:10 -0500

    chore(plugin/hop): disable hop forward command

--- after/plugin/hop.rc.lua
+++ after/plugin/hop.rc.lua
@@ -1,11 +1,15 @@
 local status, hop = pcall(require, "hop")
 if (not status) then return end
 
+hop.setup()
+
 local directions = require('hop.hint').HintDirection
 
+--[[
 vim.keymap.set('', 'f', function()
   hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
 end, {remap=true})
+--]]
 
 vim.keymap.set('', 'F', function()
   hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
